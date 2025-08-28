ee4644c27e2cac1f34838cff357b2227c5babba8
Tagforaz <fordeveloper97@gmail.com>
2025-08-28 18:18:35 +0400

    Initial commit

---
+++ 28.08MiniProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _28._08MiniProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
